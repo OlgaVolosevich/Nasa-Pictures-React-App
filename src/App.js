2dee02b073f647be98dc1a5dd8aeb4b30c7a10c9
Olga Volosevich <olga.lucky.v@gmail.com>
2020-06-20 11:59:42 +0300

    feat: add calendar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
+import CalendarContainer from "./components/Calendar";
 
 function App() {
+  const [date, getDate] = useState(new Date().toLocaleDateString());
   return (
     <div className="App">
-     
+      <h1>Pick any date please</h1>
+      <CalendarContainer getDate={getDate} />
+      <h2>Picked date: {date}</h2>
     </div>
   );
 }
---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./calendar.scss";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+const CalendarContainer = ({ getDate }) => {
+  const [date, setDate] = useState(new Date());
+  const onChangeDate = (date) => {
+    setDate(date);
+    getDate(date.toLocaleDateString());
+  };
+
+  return (
+    <div className="calendar">
+      <Calendar onChange={onChangeDate} value={date} />
+    </div>
+  );
+};
+
+export default CalendarContainer;
