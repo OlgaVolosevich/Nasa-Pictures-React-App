083c87a5c6c64bb884f44aa61fb4ee62fad2cc29
Olga Volosevich <olga.lucky.v@gmail.com>
2020-06-21 11:21:10 +0300

    Updates

---
+++ precache-manifest.45f98b85988d9ea5e987603b452e2abb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e972ec9cc339e3c6114eb3a82c273019",
+    "url": "/NASA-Pictures-React-App/index.html"
+  },
+  {
+    "revision": "359040561dca3a4c2b37",
+    "url": "/NASA-Pictures-React-App/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "861141272d684a7065a5",
+    "url": "/NASA-Pictures-React-App/static/css/main.4b8d980f.chunk.css"
+  },
+  {
+    "revision": "359040561dca3a4c2b37",
+    "url": "/NASA-Pictures-React-App/static/js/2.37081630.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASA-Pictures-React-App/static/js/2.37081630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861141272d684a7065a5",
+    "url": "/NASA-Pictures-React-App/static/js/main.61451b37.chunk.js"
+  },
+  {
+    "revision": "fc58b97e88fdd94548bd",
+    "url": "/NASA-Pictures-React-App/static/js/runtime-main.618466a6.js"
+  },
+  {
+    "revision": "3ff99de1433b96456fe87c7f540bac9f",
+    "url": "/NASA-Pictures-React-App/static/media/preloader.3ff99de1.svg"
+  }
+]);
